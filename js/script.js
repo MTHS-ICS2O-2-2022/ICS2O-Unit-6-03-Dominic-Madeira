3ae807a901990234bf380ca15af30de83549dbb9
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-06-03 01:38:17 +0000

    finished start

--- js/script.js
+++ js/script.js
@@ -8,31 +8,33 @@
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit-6-03-Dominic-Madeira/sw.js", {
-    scope: "/ICS2O-Unit-1-08-Dominic-Madeira/",
+    scope: "/ICS2O-Unit-6-03-Dominic-Madeira/sw.js",
   })
 }
 
-const getImage = async (URLAddress) => {
+/**
+ * Get API info.
+ */
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getWeather = async (URLAddress) => {
   try {
     const result = await fetch(URLAddress)
     const jsonData = await result.json()
     console.log(jsonData)
-    document.getElementById("api-image").innerHTML =
-    '<img src="' + 
-      jsonData.url + 
-      '" alt="API image" class="center" ' +
-      '>'
-    if (jsonData.main.temp != "none") {
-    
-  } else {
-    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
-  }
-  if (jsonData.weather.icon != "none") {
-    document.getElementById("image-date").innerHTML = "<p>Date: unknown</p>"
-  }
+    // Get weather and convert from kelvin to celsius
+    const weather = Math.round(jsonData.main.temp - 273.15)
+    document.getElementById("api-weather").innerHTML =
+      "<p>Temperature: " + weather + "°C</p>"
+    // Get weather icon
+    const icon = jsonData.weather[0].icon
+    const iconURL = "http://openweathermap.org/img/w/" + icon + ".png"
+    document.getElementById("api-icon").innerHTML = "<img src='" + iconURL + "' class='donut'></img>"
   } catch (err) {
     console.log(err)
   }
 }
 
-getImage("https://api.catboys.com/img")
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
